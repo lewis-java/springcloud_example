22072d463b4970ec841e390a0adafe3b338888e4
ganlin.liu <465317245@qq.com>
2019-03-15 16:31:58 +0800

    [fix] common fix’

--- springcloud-user/src/test/java/com/lgl/study/user/UserApplicationTests.java
+++ springcloud-user/src/test/java/com/lgl/study/user/UserApplicationTests.java
@@ -7,12 +7,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class UserApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void thread() {
+        ScheduledThreadPoolExecutor ss = new ScheduledThreadPoolExecutor(10);
+        ss.scheduleWithFixedDelay(
+                () -> System.out.println("sss"),
+                0L,
+                100L,
+                TimeUnit.MILLISECONDS
+        );
     }
 
 }
